d3083a611d69ac2c651e6c0e93d118f804d9d167
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2019-11-18 11:16:23 +0000

    Agora vamos começar a serio

--- Projeto/Person.java
+++ Projeto/Person.java
@@ -1,5 +1,48 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Person {
-    private String name, email;
+    //Variables
+    private  String name, email;
+    private ArrayList<Task> tasks;
+
+    //Constructor
+    Person(String name, String email){
+        this.name=name;
+        this.email=email;
+    }
+    //Getter - Setter
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    //Other Functions
+
+
+    //toString
+    @Override
+    public String toString() {
+        String out = "";
+        //TODO: funçao
+        return out;
+    }
 }
+
