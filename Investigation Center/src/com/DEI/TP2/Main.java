7487ea556319d24dd2343e8d68da4fd908a3f26e
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2019-11-18 11:56:29 +0000

    Num XuPo

--- Investigation Center/src/com/DEI/TP2/Main.java
+++ Investigation Center/src/com/DEI/TP2/Main.java
@@ -12,9 +12,6 @@ public class Main {
         Date begin = new Date(100,0,5,10,10);
         @Deprecated
         Date end = new Date(100,1,8,10,10);
-        //incriiiiiiiiiiiivel
-        //PONEI PONEI PONEI CHUPA ME A GAITA
-        //CHUPA CHUPA ATE FAZER FERIDA
         ArrayList<Person> pess = new ArrayList<Person>();
 
         cisuc.addProject(new Project("P1","proj",begin,end,10,pess));
