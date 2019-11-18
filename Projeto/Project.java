32e3352976388c16ca7a6413e4d89c87b8d1e3b8
brunofaria1322 <brunofaria1322@users.noreply.github.com>
2019-11-18 10:51:37 +0000

    implementaçao
    yeeet

---
+++ Projeto/Project.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Project {
+    //variables
+    private String name, acronym;
+    private Date beginDate, endDate;
+    private int duration;               //months
+    private ArrayList<Person> members;
+
+    //Functions
+
+}
